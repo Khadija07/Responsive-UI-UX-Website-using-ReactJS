886130d609660579ef40b3393e6e97ee5b864cf9
Khadijatul Kobra <ktk_01@outlook.com>
2024-06-17 16:16:52 +0300

    Header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Header.css'
 
+import Illustration from '../../assets/Illustration.png'
+import people from '../../assets/Group 81.png'
+
+
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header__padding'id='home' >
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__peoples'>
+          <img src={people}/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+
+        </div>
+
+        <div className='gpt3__header-ai'>
+          <img src= {Illustration} alt='AI'/>
+        </div>
+      </div>
+      
     </div>
   )
 }
